b9b654f6cb2a0c56f44870f6c05a6f1f5872b7dd
Nickelfax <nickelolei@hotmail.com>
2026-05-09 05:41:30 +0200

    Create sercice-worker.js

---
+++ sercice-worker.js
@@ -0,0 +1,17 @@
+const CACHE = 'abc-wort-v1';
+const FILES = [
+  '/ABC-Wort/',
+  '/ABC-Wort/index (2).html',
+  '/ABC-Wort/icon-512.png',
+  '/ABC-Wort/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
